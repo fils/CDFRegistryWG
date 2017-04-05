630eba31dc15bd624692f4bf817bd9e5219a331e
Douglas Fils <dfils@oceanleadership.org>
2017-04-05 16:42:14 -0500

    JSON-LD builder test

---
+++ schemaBuilder/README.md
@@ -0,0 +1,10 @@
+# schema builder
+
+## About
+A simple program that right now just sets values in a struct and 
+converts this to valid JSON-LD using schema.org vocabulary.  
+
+Finishing out the struct based on formalizing the terms and 
+references for a repository would be rather easy.  Once done 
+adding a simple form interface and allowing people to populate the results 
+and obtain a generated JSON-LD is straightforward. 
---
+++ schemaBuilder/main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/kazarena/json-gold/ld"
+)
+
+type SchemaOrgMetadata struct {
+	Context      Context      `json:"@context"` // was []interface{}  should be Context struct (which has 3 items in it for each voc)
+	Type         string       `json:"@type"`
+	ID           string       `json:"@id"` // need an ID for each of these subsections
+	URL          string       `json:"url"`
+	ContactPoint ContactPoint `json:"contactPoint"`
+}
+
+type Context struct {
+	Schema   string `json:"@vocab"`
+	GeoLink  string `json:"geolink"`         // namespace prefix in the rest of the struct
+	OpenCore string `json:"opencore"`        // namespace prefix in the rest of the struct
+	Base     string `json:"@base,omitempty"` // used in CSVW to prevent relative IRI from becoming an absolute IRI
+}
+
+type ContactPoint struct {
+	Type        string `json:"@type"`
+	ID          string `json:"@id"`
+	Name        string `json:"name"`
+	URL         string `json:"url"`
+	Email       string `json:"email"`
+	ContactType string `json:"contactType`
+}
+
+func main() {
+
+	result := SchemaOrgMetadata{}
+	result.URL = "http://example.org"
+	result.Type = "Organization"
+	result.ID = fmt.Sprintf("%s", result.URL) //  %s.json ??
+
+	// context setting
+	result.Context.Schema = "http://schema.org/" // this "schema" is the @voc in the struct..  confusing when not using schema.org
+	result.Context.OpenCore = "http://opencore.org/voc/1/"
+	result.Context.GeoLink = "http://glview.org/voc/1/"
+	result.Context.Base = fmt.Sprintf("%s", result.URL) //  %s.json
+
+	// contactPoint setting
+	result.ContactPoint.Type = "ContactPoint"
+	result.ContactPoint.ID = "ID"
+	result.ContactPoint.Name = "John Doe"
+	result.ContactPoint.URL = "http://url.to/person"
+	result.ContactPoint.Email = "joe@example.org"
+	result.ContactPoint.ContactType = "technical support"
+
+	jsonldtext, _ := json.MarshalIndent(result, "", " ") // results as embeddale JSON-LD
+
+	fmt.Println("jsonld text--------------------------------")
+	fmt.Println(string(jsonldtext))
+
+	// fmt.Println("jsonLDToRDF--------------------------------")
+	// fmt.Println(jsonLDToRDF(string(jsonldtext)))
+
+}
+
+// Trys to take a simple JSON-LD string and process it to RDF.
+func jsonLDToRDF(jsonld string) string {
+
+	proc := ld.NewJsonLdProcessor()
+	options := ld.NewJsonLdOptions("")
+	options.Format = "application/nquads"
+
+	var myInterface interface{}
+	err := json.Unmarshal([]byte(jsonld), &myInterface)
+	if err != nil {
+		log.Println("Error when transforming JSON-LD document to interface:", err)
+	}
+
+	triples, err := proc.ToRDF(myInterface, options)
+	if err != nil {
+		log.Println("Error when transforming JSON-LD document to RDF:", err)
+		return err.Error()
+	}
+
+	return triples.(string)
+}
